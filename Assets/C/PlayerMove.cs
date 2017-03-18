b3c425e3e7614553aaa7bf70c27c69b362ec2a38
NEW\Miki <nattakan.eimo@bumail.net>
2017-03-18 12:57:38 +0700

    AddMove

---
+++ Assets/C/PlayerMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+	public float moveSpeed;
+	float mhorizontal = Input.GetAxis("Horizontal");
+	float mvertical = Input.GetAxis("Vertical");
+
+	// Use this for initialization
+	void Start () {
+		moveSpeed = 10f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (moveSpeed*mhorizontal*Time.deltaTime,0f,moveSpeed*mvertical*Time.deltaTime);
+	}
+}
